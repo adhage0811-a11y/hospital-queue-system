d2011881356c33fe004b54504ce0d13c73997ee3
adhage0811-a11y <adhage0811@gmail.com>
2026-03-03 11:58:28 +0530

    Add patient management functions to script.js
    Signed-off-by: adhage0811-a11y <adhage0811@gmail.com>

---
+++ script.js
@@ -0,0 +1,31 @@
+const backendURL = "https://hospital-backend-t8lx.onrender.com";
+
+async function addPatient() {
+    const name = document.getElementById("name").value;
+
+    if (name === "") {
+        alert("Enter Name");
+        return;
+    }
+
+    const response = await fetch(`${backendURL}/token`);
+    const data = await response.json();
+
+    alert("Your Token: " + data.token);
+
+    document.getElementById("name").value = "";
+    loadCurrent();
+}
+
+async function nextPatient() {
+    alert("Next button pressed (backend demo)");
+}
+
+async function loadCurrent() {
+    const response = await fetch(`${backendURL}/current`);
+    const data = await response.json();
+
+    document.getElementById("current").innerText = data.currentToken;
+}
+
+window.onload = loadCurrent;
