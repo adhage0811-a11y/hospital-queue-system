7322e4684dab86b81372aace2223a950d5b6b95e
adhage0811-a11y <adhage0811@gmail.com>
2026-03-05 12:15:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,42 +2,60 @@ let queue = [];
 let current = 0;
 
 function addPatient() {
-  let name = document.getElementById("name").value;
 
-  if (name === "") {
-    alert("Enter name");
-    return;
-  }
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
 
-  let token = queue.length + current + 1;
-  queue.push({ name: name, token: token });
-
-  alert("Your Token: " + token);
-
-  document.getElementById("name").value = "";
-  showQueue();
+if(name === "" || phone === "") {
+alert("Enter name and mobile number");
+return;
 }
 
-function nextPatient() {
-  if (queue.length === 0) {
-    alert("No patients");
-    return;
-  }
+let token = queue.length + current + 1;
 
-  let patient = queue.shift();
-  current = patient.token;
+queue.push({
+name:name,
+phone:phone,
+token:token
+});
 
-  document.getElementById("current").innerText = current;
-  alert(patient.name + ", your turn!");
+alert("Token Generated: " + token);
 
-  showQueue();
+document.getElementById("name").value = "";
+document.getElementById("phone").value = "";
+
+showQueue();
 }
 
-function showQueue() {
-  let text = "";
-  queue.forEach(function(p) {
-    text += "Token " + p.token + " - " + p.name + "<br>";
-  });
+function nextPatient(){
 
-  document.getElementById("list").innerHTML = text;
+if(queue.length === 0){
+alert("No patients waiting");
+return;
+}
+
+let patient = queue.shift();
+
+current = patient.token;
+
+document.getElementById("current").innerText = current;
+
+alert("SMS sent to " + patient.phone +
+"\nHello " + patient.name +
+"\nYour Token " + patient.token + " is now active.");
+
+showQueue();
+
+}
+
+function showQueue(){
+
+let text = "<h3>Waiting List</h3>";
+
+queue.forEach(p => {
+text += "Token " + p.token + " - " + p.name + "<br>";
+});
+
+document.getElementById("list").innerHTML = text;
+
 }
